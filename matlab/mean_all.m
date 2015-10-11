b1a2043f44384929eac5447e6b4f06f8e6aebee8
servold <servold@gmail.com>
2015-10-11 11:39:30 +0300

    mean_all helper function

---
+++ matlab/mean_all.m
@@ -0,0 +1,22 @@
+function [mean_iters,mean_time,mean_psi,mean_delta_x] = mean_all(trials,max_iters,iters,time,Psi,delta_x)
+    mean_iters = mean(iters,3);
+    mean_time = mean(time,3);
+    s = size(iters);
+    algs = s(1);
+    no_nans_psi = Psi;
+    no_nans_delta_x = delta_x;
+    for i = 1:algs
+        for j = 1:trials
+            for k = 2:max_iters
+                if(isnan(no_nans_psi(i,k,j)))
+                    no_nans_psi(i,k,j) = no_nans_psi(i,k-1,j);
+                end
+                if(isnan(no_nans_delta_x(i,k,j)))
+                    no_nans_delta_x(i,k,j) = no_nans_delta_x(i,k-1,j);
+                end
+            end
+        end
+    end
+    mean_psi = mean(no_nans_psi,3);
+    mean_delta_x = mean(no_nans_delta_x,3);
+end
